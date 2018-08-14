3c410b50f50cd35247783f1f5d3a390a3ed0693d
_dream <will.ferens@gmail.com>
2018-08-14 16:20:52 -0600

    added JMT start coordinates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
-//import MapGL from 'react-map-gl'
-import './App.css';
+import React, { Component } from 'react'
+import MapGL from 'react-map-gl'
+import './App.css'
 
-//const token = "pk.eyJ1Ijoid2lsbC1mZXJlbnMiLCJhIjoiY2pra2o0ZDFmMGlybzNxcGowMnBkOGcwOCJ9.SakByvxcSSfCGnlG5A8MbQ"
-
+const token = "pk.eyJ1Ijoid2lsbC1mZXJlbnMiLCJhIjoiY2pra2o0ZDFmMGlybzNxcGowMnBkOGcwOCJ9.SakByvxcSSfCGnlG5A8MbQ"
+//lat 
+//lon 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+      <MapGL
+        width={700}
+        height={450}
+        latitude={37.73944444}
+        longitude={-119.57305556}
+        zoom={9.017}
+
+        mapboxApiAccessToken={token}
+      />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
