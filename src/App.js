fa1d66a58ac0ae51275eea0efcfad6f9b4ce3eeb
_dream <will.ferens@gmail.com>
2018-08-14 16:24:27 -0600

    added readme

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import MapGL from 'react-map-gl'
 import './App.css'
 
 const token = "pk.eyJ1Ijoid2lsbC1mZXJlbnMiLCJhIjoiY2pra2o0ZDFmMGlybzNxcGowMnBkOGcwOCJ9.SakByvxcSSfCGnlG5A8MbQ"
-//lat 
-//lon 
+
 class App extends Component {
   render() {
     return (
