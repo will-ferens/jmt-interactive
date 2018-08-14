5e5c5438c6a768674a3da5d19f438adb4bb6c048
_dream <will.ferens@gmail.com>
2018-08-14 16:09:07 -0600

    initial commit, resolved mapbox issues with create react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import MapGL from 'react-map-gl'
+import './App.css';
+
+//const token = "pk.eyJ1Ijoid2lsbC1mZXJlbnMiLCJhIjoiY2pra2o0ZDFmMGlybzNxcGowMnBkOGcwOCJ9.SakByvxcSSfCGnlG5A8MbQ"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
+}
+
+export default App;
